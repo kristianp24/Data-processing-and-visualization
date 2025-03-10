e37907a2a2273eda4d6f951d2d988c4185be2d29
kristianp24 <kristianprendi29@gmail.com>
2025-03-10 12:06:25 +0200

    filtrare jucatori dupa echipa si tara

--- streamlit/pages/Filtrarari si Afisare Date.py
+++ streamlit/pages/Filtrarari si Afisare Date.py
@@ -47,14 +47,26 @@ varsta = st.slider("Alegeti o varsta", min_value=12, max_value=50)
 # varsta trebuie sa o calculam, in tabel avem doar anul nasterii sub forma 1978-06-09
 if varsta:
     players["age"] = 2025 - pd.to_datetime(players["date_of_birth"]).dt.year
-    st.write(players[["name", "current_club_name", "age"]][players["age"] == varsta])
+    st.write(players[["name", "current_club_name", "age","market_value_in_eur"]][players["age"] == varsta])
 
 st.markdown("### Cei mai valorosi jucatori")
 st.write(players[["name", "current_club_name", "highest_market_value_in_eur"]].sort_values(by="highest_market_value_in_eur", ascending=False).head(10))
 
 
+# Afisare jucatori din o echipa
+st.markdown("### Afisare jucatorii dintr-o echipa")
+club = st.selectbox("Alegeti o echipa", sorted(players["current_club_name"].unique().tolist()))
+checkbox = st.checkbox("Afiseaza doar cei din ultimul an", value=False)
+if club and checkbox:
+    st.write(players.loc[(players["current_club_name"] == club) & (players["last_season"] == 2024), ["name", "position", "current_club_name", "last_season","market_value_in_eur"]])
+elif club:
+    st.write(players.loc[players["current_club_name"] == club, ["name", "position", "current_club_name", "last_season","market_value_in_eur"]])  
 
-
+# Filtrare jucatori dupa tara de Origina
+st.markdown("### Filtrare jucatori dupa tara de origine")
+tara = st.selectbox("Alegeti o tara", players["country_of_birth"].unique().tolist())
+if tara:
+    st.write(players.loc[players["country_of_birth"] == tara, ["name", "position", "current_club_name", "country_of_birth","last_season","market_value_in_eur"]])
 
 
 
